6ba218290ffcd099021daa6ce781931ad6dfc4ac
Faraneh Javadinejad <faraneh.javadinejad@points.com>
2024-08-08 08:36:56 -0400

    modal-applying-on-all-app-issue

--- apps/ttt-nx/src/app/app.tsx
+++ apps/ttt-nx/src/app/app.tsx
@@ -1,12 +1,13 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import MainPage from '../components/mainPage/mainPage';
-import { Styles } from '@my-org/styles'
+import * as React from 'react';
+import MainPage from '../components/mainPage/mainPage.tsx';
+// import { Styles } from '../../../../libs/styles/styles/src/lib/styles'
 
 // import NxWelcome from './nx-welcome';
 
 export function App() {
   return (
-    <div className={Styles()}>
+    <div>
       {/* <NxWelcome title="ttt-nx" /> */}
       <MainPage />
     </div>
